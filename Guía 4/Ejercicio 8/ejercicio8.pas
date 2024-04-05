264c892cb9b1f01511deb3379d8a5d5700ff1e16
carluandraster <99754800+carluandraster@users.noreply.github.com>
2024-04-05 10:36:53 -0300

    Add files via upload

--- Guía 4/Ejercicio 8/ejercicio8.pas
+++ Guía 4/Ejercicio 8/ejercicio8.pas
@@ -21,60 +21,48 @@ devuelva el correspondiente descuento. }
 Function Descuento(tipo:char;dia:byte;monto:real):   real;
 Begin
     If (dia=10) Or (dia=20) Or (dia=30) And (tipo='O') And (monto>300) Then
-        Descuento := 0.05*monto
+        Descuento := 0.05
     Else
         If (tipo='C') And (dia>=15) Then
-            Descuento := 0.1*monto
+            Descuento := 0.1
     Else
         If (tipo='S') And (dia<15) Then
-            Descuento := 0.15*monto
+            Descuento := 0.15
     Else
         Descuento := 0;
 End;
 
-Function extraerTipo(cadena:string):char;
-    begin
-      extraerTipo:=cadena[1];
-    end;
-
-Function extraerDia(cadena:string):byte;
+Procedure cortar(cadena:string;var tipo:char; var dia:byte; var monto:real);
     var
+        i:byte;
         diaStr:string;
-        i,n:byte;
     begin
-        i:=3;
-        diaStr:='';
-        while cadena[i]<>' ' do
-            begin
-              diaStr:=diaStr+cadena[i];
-              i:=i+1;
-            end;
-            
-        val(diaStr,n);
-        extraerDia:=n;
-    end;
-
-Function extraerMonto(cadena:string):real;
-    var
-        i,cuentaBlancos:byte;
-        n:word;
-    begin
-        i:=0;
-        cuentaBlancos:=0;
-        repeat
-          i:=i+1;
-          if cadena[i]=' ' then
-            cuentaBlancos:=cuentaBlancos+1;
-        until cuentaBlancos=2;
-        delete(cadena,1,i);
-        val(cadena,n);
-        extraerMonto:=n;
+        // Extraer tipo de rubro
+            tipo:=cadena[1];
+            delete(cadena,1,2);
+        
+        if tipo<>'F' then
+          begin
+            // Extraer dia
+                i:=1;
+                diaStr:='';
+                while cadena[i]<>' ' do
+                    begin
+                        diaStr:=diaStr+cadena[i];
+                        i:=i+1;
+                    end;
+                delete(cadena,1,i);
+                val(diaStr,dia);
+            // Extraer monto
+                val(cadena,monto);
+          end;
+        
     end;
 
 Var 
     tipo:   char;
     dia:   byte;
-    monto,totalAbonado,ahorro:   real;
+    monto,totalAbonado,ahorro,desc:   real;
     archivo:   text;
     descuentoTresRubros,lectura,codigo:   string;
     cuentaClientes:   word;
@@ -98,21 +86,23 @@ Begin
 
             // Leer compras
             readLn(archivo,lectura);
-            tipo:=extraerTipo(lectura);
+            cortar(lectura,tipo,dia,monto);
             While tipo<>'F' Do
                 Begin
-                    monto:=extraerMonto(lectura);
-                    totalAbonado := totalAbonado+monto;
-                    dia:=extraerDia(lectura);
-                    ahorro := ahorro+Descuento(tipo,dia,monto);
-                    If Descuento(tipo,dia,monto)<>0 Then
-                        Case tipo Of 
-                            'C':   descuentoTresRubros[1] := '1';
-                            'S':   descuentoTresRubros[2] := '1';
-                            'O':   descuentoTresRubros[3] := '1';
-                        End;
-                    read(archivo,lectura);
-                    tipo:=extraerTipo(lectura);
+                    // Procesar compra
+                        totalAbonado := totalAbonado+monto;
+                        desc:=Descuento(tipo,dia,monto);
+                        ahorro := ahorro+monto*desc;
+                        If desc<>0 Then
+                            Case tipo Of 
+                                'C':   descuentoTresRubros[1] := '1';
+                                'S':   descuentoTresRubros[2] := '1';
+                                'O':   descuentoTresRubros[3] := '1';
+                            End;
+                    
+                    // Siguiente compra
+                        read(archivo,lectura);
+                        cortar(lectura,tipo,dia,monto);
                 End;
 
             // a) Código del cliente y total abonado, por cada cliente que no se benefició con ningún descuento.
